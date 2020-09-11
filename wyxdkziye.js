2cb0541052a577b66f3e8e26e83472a351f467d9
ziye12 <59244461+ziye12@users.noreply.github.com>
2020-09-11 17:21:20 +0800

    Update wyxdkziye.js

--- wyxdkziye.js
+++ wyxdkziye.js
@@ -1,18 +1,19 @@
-/*ziye
+/*ziye  玩游戏打卡
 
-微信扫码  https://raw.githubusercontent.com/ziye12/JavaScript/master/wyxdkziye.png 进入小程序，登录获取授权
+非常重要！！！   
+
+微信扫码  https://raw.githubusercontent.com/ziye12/JavaScript/master/asdkziye.png 进入小程序，登录获取授权，再启动软件获取ck 
+建议设置6点到23点之间打卡，时间间隔30分钟以上,否则容易封号
 
 
 
 2020.9.9
 
 
-说明：以玩游戏打卡为例的打卡模版，可以抓包然后替换以下内容来制作其他打卡脚本
+《《《《《《《《《《制作其他打卡js的说明
 
-
-需要替换的内容如下，先替换长字符的，再替换短字符的，自己用电脑或者KODE软件替换，KODE下载链接https://apps.apple.com/cn/app/koder-code-editor/id1447489375
-
-
+抓包小程序，然后替换以下内容来制作其他打卡js，
+需要替换的内容如下，按顺序替换字符，自己用电脑或者KODE软件替换，KODE下载链接https://apps.apple.com/cn/app/koder-code-editor/id1447489375
 
 we7.ivee.top
 玩游戏打卡
@@ -24,6 +25,15 @@ wyxdk
 
 手动替换
 we7\.ivee\.top
+
+制作其他打卡js的说明》》》》》》》》》》》
+
+
+
+
+
+
+////////////////////////////////////////正文
 
 使用说明：
 
@@ -39,12 +49,10 @@ we7\.ivee\.top
 3.现金统计，自动提现
 
 
-***建议设置每小时运行一次 
 
 
-圈X
 
-1、hostname= we7.ivee.top,
+hostname= we7.ivee.top,
 
 
 
@@ -62,14 +70,9 @@ http-request ^https:\/\/we7\.ivee\.top\/* script-path= https://raw.githubusercon
 , requires-body=true, timeout=10, tag=玩游戏打卡
 
 
-
-
-
-
 */
 
 
-//正文
 
 
 const sy = init()//声明必须
@@ -77,7 +80,7 @@ const notifyInterval = 1; //通知开为1，常关为0
 const logs = 0; // 日志开关
 const jsname = '玩游戏打卡'
 const jbid =1
-const sb='失败,请获取ck'
+const sb='失败,请先关闭，进入程序获取授权'
 
 const cs = 5;//小程序打卡次数
 const xj = 1;//提现标准
