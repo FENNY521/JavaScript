18dd09127ca4274af87ed9406b60adf0c31234a4
ziye12 <59244461+ziye12@users.noreply.github.com>
2020-09-11 17:28:32 +0800

    Update wyxdkziye.js

--- wyxdkziye.js
+++ wyxdkziye.js
@@ -2,7 +2,7 @@
 
 非常重要！！！   
 
-微信扫码  https://raw.githubusercontent.com/ziye12/JavaScript/master/asdkziye.png 进入小程序，登录获取授权，再启动软件获取ck 
+微信扫码  https://raw.githubusercontent.com/ziye12/JavaScript/master/wyxdkziye.png 进入小程序，登录获取授权，再启动软件获取ck 
 建议设置6点到23点之间打卡，时间间隔30分钟以上,否则容易封号
 
 
